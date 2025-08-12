7c8ab0722961aa6cdfb3055cd48b93efa36b36c3
Mamkinhecker <alsevskijmatvej@gmail.com>
2025-08-12 11:19:04 +0300

    brainrot

--- http-server/handlers/url/save/save_test/save_test.go
+++ http-server/handlers/url/save/save_test/save_test.go
@@ -7,7 +7,13 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"url-shortener/http-server/handlers/url/save"
 	"url-shortener/http-server/handlers/url/save/mocks"
+
+	//"url-shortener/lib/logger/sl"
+	"errors"
+
+	"url-shortener/handlers/slogdiscrad"
 
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -26,6 +32,30 @@ func TestSaveHandler(t *testing.T) {
 			alias: "test_alias",
 			url:   "https://google.com",
 		},
+		{
+			name:  "Empty alias",
+			alias: "",
+			url:   "https://google.com",
+		},
+		{
+			name:      "Empty URL",
+			url:       "",
+			alias:     "some_alias",
+			respError: "field URL is a required field",
+		},
+		{
+			name:      "Invalid URL",
+			url:       "some invalid URL",
+			alias:     "some_alias",
+			respError: "field URL is not a valid URL",
+		},
+		{
+			name:      "SaveURL Error",
+			alias:     "test_alias",
+			url:       "https://google.com",
+			respError: "failed to add url",
+			mockError: errors.New("unexpected error"),
+		},
 	}
 
 	for _, tc := range cases {
@@ -36,7 +66,7 @@ func TestSaveHandler(t *testing.T) {
 					Return(int64(1), tc.mockError).
 					Once()
 			}
-			handler := save.New(sl.NewDiscardLogger(), urlSaverMock)
+			handler := save.New(slogdiscrad.NewDiscardLogger(), urlSaverMock)
 			input := fmt.Sprintf(`{"url": "%s", "alias": "%s"}`, tc.url, tc.alias)
 			req, err := http.NewRequest(http.MethodPost, "/save", bytes.NewReader([]byte(input)))
 			require.NoError(t, err)
@@ -44,7 +74,7 @@ func TestSaveHandler(t *testing.T) {
 			handler.ServeHTTP(rr, req)
 			require.Equal(t, rr.Code, http.StatusOK)
 			body := rr.Body.String()
-			var resp save.Response
+			var resp save.Responce
 			require.NoError(t, json.Unmarshal([]byte(body), &resp))
 			require.Equal(t, tc.respError, resp.Error)
 		})
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"errors"
 	"log/slog"
+	"net/http"
 	"os"
 	"url-shortener/config"
 	"url-shortener/http-server/handlers/url/save"
+	"url-shortener/lib/api/responce"
 	"url-shortener/lib/logger/sl"
+	"url-shortener/storage"
 	"url-shortener/storage/sqlite"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 const (
@@ -17,6 +22,10 @@ const (
 	envDev   = "dev"
 	envProd  = "prod"
 )
+
+type URLGetter interface {
+	GetURL(alias string) (string, error)
+}
 
 func main() {
 	cfg := config.Mustload()
@@ -34,7 +43,43 @@ func main() {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
-	router.Post("/", save.New(log, storage))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage))
+	})
+	router.Get("/{alias}", redirect.New(log, storage))
+}
+
+func New(log *slog.Logger, urlGetter URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.redirect.New"
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+			render.JSON(w, r, responce.Error("not found"))
+			return
+		}
+		resURL, err := urlGetter.GetURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+			render.JSON(w, r, responce.Error("not found"))
+			return
+		}
+		if err != nil {
+			log.Error("failed to get url", sl.Err(err))
+			render.JSON(w, r, responce.Error("internal error"))
+			return
+		}
+		log.Info("got url", slog.String("url", resURL))
+		http.Redirect(w, r, resURL, http.StatusFound)
+	}
 }
 
 func setupLogger(env string) *slog.Logger {
